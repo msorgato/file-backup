7f4844446b3e7d60fcf9a7874f8d9508e01ccb5a
msorgato <mattia.sorgato@yahoo.com>
2022-12-17 19:21:23 +0100

    Lettura path sorgente dalle config

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.apache.commons.configuration.*;
+
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws ConfigurationException {
         System.out.println("Hello world!");
+
+//        ConfigurationFactory factory = new ConfigurationFactory("config.properties");
+//        Configuration config = factory.getConfiguration();
+
+        CompositeConfiguration config = new CompositeConfiguration();
+        config.addConfiguration(new SystemConfiguration());
+        config.addConfiguration(new PropertiesConfiguration("config.properties"));
+
+        System.out.println("Percorso iniziale: '" + config.getProperty("backup.source.base-path") + "'");
     }
 }
